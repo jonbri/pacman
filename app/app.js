c9a07facdb66ea32754f7a41469c7525591c99df
Jonathan Brink <jonathandavidbrink@gmail.com>
2022-06-28 11:01:17 -0400

    wip

--- app/app.js
+++ app/app.js
@@ -13,6 +13,7 @@ const App = {
   level: 1,
   points: 0,
   killedAggregate: [],
+  pointsAggregate: [0,0],
   killCount: 0,
   deathsCount: 0,
   fruitCount: 0,
@@ -26,6 +27,20 @@ const App = {
   },
   set: (key, value) => {
     App[key] = value;
+    if (key === 'points') {
+      if (App.pointsAggregate.length <= App.level) {
+        App.pointsAggregate.push(0);
+      }
+      let previousLevelPoints = 0;
+      if (App.level > 1) {
+        previousLevelPoints = App.pointsAggregate[App.level - 1];
+      }
+      let perLevel = App.points - previousLevelPoints;
+      if (!perLevel) {
+        perLevel = 0;
+      }
+      App.pointsAggregate[App.level] = perLevel;
+    }
     App.render();
   },
   incrementKills: () => {
@@ -51,21 +66,27 @@ const App = {
         killedLog.removeChild(killedLog.firstChild);
       }
     }
+    function generateXString(howMany) {
+      let xString = '';
+      for (var i = 0; i < howMany; i++) {
+        xString += "x";
+      }
+      return xString;
+    }
     function printTable() {
       clearTable();
       for (var i = 1; i <= App.level; i++) {
         var tr = document.createElement('tr')
         var td0 = document.createElement('th')
         var td1 = document.createElement('td')
-        td0.innerHTML = `${i} `;
+        var td2 = document.createElement('td')
+        td0.innerHTML = `${i}`;
         var howManyPerLevel = App.killedAggregate.filter((x) => x === i).length;
-        var xString = '';
-        for (var j = 0; j < howManyPerLevel; j++) {
-          xString += "x";
-        }
-        td1.innerHTML = xString;
+        td1.innerHTML = generateXString(howManyPerLevel);
+        td2.innerHTML = `(${App.pointsAggregate[i]})`;
         tr.append(td0)
         tr.append(td1)
+        tr.append(td2)
         killedLog.append(tr);
       }
     }
