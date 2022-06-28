e2abf7eb01cbbd8138b5e3e1c0d2eafb734b7a11
Jonathan Brink <jonathandavidbrink@gmail.com>
2022-06-27 21:43:47 -0400

    scope fix

--- app/timer.js
+++ app/timer.js
@@ -1,6 +1,7 @@
+var timerInterval;
 (function() {
   var timerCount = 0;
-  var timerInterval = setInterval(function() {
+  timerInterval = setInterval(function() {
     const mins = Math.floor(timerCount / 60);
     const secs = timerCount % 60;
     timer.innerHTML = `${mins}m  ${secs}s`;
